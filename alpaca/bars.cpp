fb7cbe152739be8a5d63d322cdc681858d84d6a3
Aidan Jalili <aidanjalili03@gmail.com>
2021-07-26 13:24:59 -0400

    Update bars.cpp

--- alpaca/bars.cpp
+++ alpaca/bars.cpp
@@ -17,7 +17,7 @@ Status Bar::fromJSON(const std::string& json) {
 
 Status Bar::fromDocument(
     const rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator>>& d) {
-  PARSE_UINT(time, "t")
+  PARSE_STRING(time, "t")
   PARSE_DOUBLE(open_price, "o")
   PARSE_DOUBLE(high_price, "h")
   PARSE_DOUBLE(low_price, "l")
