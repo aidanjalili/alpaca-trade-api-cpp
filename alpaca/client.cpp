53deca198f0f10f97536cbcc1ec955916f23326f
Aidan Jalili <aidanjalili03@gmail.com>
2021-07-25 22:05:21 -0400

    typo in last commit

--- alpaca/client.cpp
+++ alpaca/client.cpp
@@ -1087,7 +1087,7 @@ std::pair<Status, Bars> Client::getBars(const std::vector<std::string>& symbols,
   }
   auto query_string = httplib::detail::params_to_query_str(params);
 
-  auto url = "/v2/stocks/" + symbol_string + "/bars" + "?" + query_string;
+  auto url = "/v2/stocks/" + symbols_string + "/bars" + "?" + query_string;
 
   httplib::SSLClient client(environment_.getAPIDataURL());
   DLOG(INFO) << "Making request to: " << url;
