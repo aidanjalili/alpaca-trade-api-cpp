a1f5a95fa92663a8cd5dca9d064b703eff637c73
Aidan Jalili <aidanjalili03@gmail.com>
2021-07-26 10:26:35 -0400

    Update client.cpp

--- alpaca/client.cpp
+++ alpaca/client.cpp
@@ -1108,11 +1108,11 @@ std::pair<Status, Bars> Client::getBars(const std::vector<std::string>& symbols,
 
   //Make the  bar the same as it would have been in v1
   std::string real_response = resp->body;
-  int characterstoremove = 36+symbols_string.size()
+  int characterstoremove = 36+symbols_string.size();
   real_response = real_response.substr(0, -(36+symbols_string.size()));
   real_response += "}";
 
-  while((index = real_response.find("bars")) != string::npos)
+  while((index = real_response.find("bars")) != std::string::npos)
      real_response.replace(index, symbols_string.length(), symbols_string);
 
   return std::make_pair(bars.fromJSON(resp->body), bars);
