0914de8ba46cea4c289ef362539d77b2bee74807
Aidan Jalili <aidanjalili03@gmail.com>
2021-08-03 14:46:13 -0400

    Update client.cpp

--- alpaca/client.cpp
+++ alpaca/client.cpp
@@ -1110,13 +1110,7 @@ std::pair<Status, Bars> Client::getBars(const std::vector<std::string>& symbols,
   * Currently only works for one inputed stock symbol.
   */
   std::string real_response = resp->body;
-
-  if (real_response.find("null") != std::string::npos) //check to c if there is a 'null' response
-  {
-    std::ostringstream ss;
-    DLOG(INFO) << "sex" << real_response;
-    return std::make_pair(Status(1, ss.str()), bars);
-  }
+  
   int characterstoremove = 36+symbols_string.size();
   real_response.erase(real_response.size() - characterstoremove);
   real_response += "}";
@@ -1127,8 +1121,16 @@ std::pair<Status, Bars> Client::getBars(const std::vector<std::string>& symbols,
     real_response.replace(pos, 4, symbols_string);
     pos+=4;
   }
+  
+  //check for null data...
+  if (real_response.find("null") != std::string::npos)
+  {
+    std::ostringstream ss;
+    ss << "Call to " << url << " returned an empty response";
+    return std::make_pair(Status(1, ss.str()), bars);
+  }
+  
   //Re-writing of response over...
-
   DLOG(INFO) << "Response from " << url << " is effectively : " << real_response;
   return std::make_pair(bars.fromJSON(real_response), bars);
 }
